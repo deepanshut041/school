364765dce33bd020cf24712e996a04b883e891dd
deepanshut041 <deepanshut041@gmail.com>
2018-03-17 01:07:55 +0530

    Added BaseActivity now working Dependincey Injection I

---
+++ app/src/main/java/com/futurastic/deepanshu/school/BaseActivity.java
@@ -0,0 +1,48 @@
+package com.futurastic.deepanshu.school;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by deepanshu on 17/3/18.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentView());
+        onViewReady(savedInstanceState, getIntent());
+    }
+
+
+    protected void onViewReady(Bundle savedInstanceState, Intent intent){
+
+    }
+
+
+    protected void showDialog(String message){
+        if (mProgressDialog == null){
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(true);
+        }
+
+        mProgressDialog.setMessage(message);
+        mProgressDialog.show();
+    }
+
+    protected void hideDialog(){
+        if (mProgressDialog != null && mProgressDialog.isShowing()){
+            mProgressDialog.dismiss();
+        }
+    }
+
+    protected abstract int getContentView();
+}
---
+++ app/src/main/java/com/futurastic/deepanshu/school/SchoolApp.java
@@ -0,0 +1,20 @@
+package com.futurastic.deepanshu.school;
+
+import android.app.Application;
+
+/**
+ * Created by deepanshu on 17/3/18.
+ */
+
+public class SchoolApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
